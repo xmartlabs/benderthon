85694db98ea858ad09da3687739f2c8c64764d56
Santiago Castro <santi.1410@hotmail.com>
2017-06-01 17:10:43 -0300

    Fix that the MNIST sample use bias as consts and not as variables

--- sample.py
+++ sample.py
@@ -33,19 +33,19 @@ def deep_network(x):
     x_image = tf.reshape(x, [-1, 28, 28, 1])
 
     W_conv1 = tf.Variable(tf.truncated_normal([5, 5, 1, 32], stddev=0.1))
-    b_conv1 = tf.constant(0.1, shape=[32])
+    b_conv1 = tf.Variable(tf.constant(0.1, shape=[32]))
     h_conv1 = tf.nn.relu(tf.nn.conv2d(x_image, W_conv1, strides=[1, 1, 1, 1], padding='SAME') + b_conv1)
 
     h_pool1 = tf.nn.max_pool(h_conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
     W_conv2 = tf.Variable(tf.truncated_normal([5, 5, 32, 64], stddev=0.1))
-    b_conv2 = tf.constant(0.1, shape=[64])
+    b_conv2 = tf.Variable(tf.constant(0.1, shape=[64]))
     h_conv2 = tf.nn.relu(tf.nn.conv2d(h_pool1, W_conv2, strides=[1, 1, 1, 1], padding='SAME') + b_conv2)
 
     h_pool2 = tf.nn.max_pool(h_conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
     W_fc1 = tf.Variable(tf.truncated_normal([7 * 7 * 64, 1024], stddev=0.1))
-    b_fc1 = tf.constant(0.1, shape=[1024])
+    b_fc1 = tf.Variable(tf.constant(0.1, shape=[1024]))
 
     h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
@@ -54,7 +54,7 @@ def deep_network(x):
     h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
     W_fc2 = tf.Variable(tf.truncated_normal([1024, 10], stddev=0.1))
-    b_fc2 = tf.constant(0.1, shape=[10])
+    b_fc2 = tf.Variable(tf.constant(0.1, shape=[10]))
 
     y = tf.add(tf.matmul(h_fc1_drop, W_fc2), b_fc2, name="Prediction")
 
