f1dc21b4fdab6aea3b2a124fecb7ab0e122cf92d
Santiago Castro <santiago@xmartlabs.com>
2017-06-30 14:31:10 -0300

    Add flags to Caffe conversion to save the converted model and the checkpoint

--- benderthon/caffe_freeze.py
+++ benderthon/caffe_freeze.py
@@ -4,6 +4,7 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import contextlib
 import imp
 import os
 
@@ -12,17 +13,23 @@ import tensorflow as tf
 from benderthon import tf_freeze, util
 
 
-def caffe_to_tensorflow_session(caffe_def_path, caffemodel_path, inputs, graph_name='Graph'):
+@contextlib.contextmanager
+def dummy_context_mgr(obj):
+    yield obj
+
+
+def caffe_to_tensorflow_session(caffe_def_path, caffemodel_path, inputs, graph_name='Graph',
+                                conversion_out_dir_path=None):
     """Create a TensorFlow Session from a Caffe model."""
     try:
         # noinspection PyUnresolvedReferences
         from caffeflow import convert
     except ImportError:
-        raise Exception("caffe-tensorflow package needs to be installed to freeze Caffe models. Check out the README.")
+        raise Exception("caffeflow package needs to be installed to freeze Caffe models. Check out the README file.")
 
-    with util.TemporaryDirectory() as temp_dir_path:
-        params_values_output_path = os.path.join(temp_dir_path, 'params_values.npy')
-        network_output_path = os.path.join(temp_dir_path, 'network.py')
+    with (dummy_context_mgr(conversion_out_dir_path) or util.TemporaryDirectory()) as dir_path:
+        params_values_output_path = os.path.join(dir_path, 'params_values.npy')
+        network_output_path = os.path.join(dir_path, 'network.py')
 
         convert.convert(caffe_def_path, caffemodel_path, params_values_output_path, network_output_path, False)
 
@@ -37,13 +44,15 @@ def caffe_to_tensorflow_session(caffe_def_path, caffemodel_path, inputs, graph_n
         return sess
 
 
-def freeze(caffe_def_path, caffemodel_path, inputs, output_file_path, output_node_names, graph_name='Graph'):
+def freeze(caffe_def_path, caffemodel_path, inputs, output_file_path, output_node_names, graph_name='Graph',
+           conversion_out_dir_path=None, checkpoint_out_path=None):
     """Freeze and shrink the graph based on a Caffe model, the input tensors and the output node names."""
-    with caffe_to_tensorflow_session(caffe_def_path, caffemodel_path, inputs, graph_name=graph_name) as sess:
+    with caffe_to_tensorflow_session(caffe_def_path, caffemodel_path, inputs, graph_name=graph_name,
+                                     conversion_out_dir_path=conversion_out_dir_path) as sess:
         saver = tf.train.Saver()
 
-        with util.TemporaryDirectory() as temp_dir_path:
-            checkpoint_path = os.path.join(temp_dir_path, 'pose.ckpt')
+        with (dummy_context_mgr(checkpoint_out_path) or util.TemporaryDirectory()) as temp_dir_path:
+            checkpoint_path = checkpoint_out_path or os.path.join(temp_dir_path, 'pose.ckpt')
             saver.save(sess, checkpoint_path)
 
             output_node_names = util.output_node_names_string_as_list(output_node_names)
