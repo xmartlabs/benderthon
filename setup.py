a79ecebb8229406b1dd362c82ea5e98aa3d36177
Santiago Castro <santiago@xmartlabs.com>
2017-07-24 10:58:10 -0300

    Bump version 0.2.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import pypandoc
 
 setup(
     name="benderthon",
-    version="0.2.0",
+    version="0.2.1",
     description="Set of utilities to work easier with Bender.",
     long_description=pypandoc.convert('README.md', 'rst'),
     url="https://github.com/xmartlabs/benderthon",
